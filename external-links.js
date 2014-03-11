e8d262d7ba236af1e6c4b9b19c41c661de68d1f3
Matt Walker <walkermatt@longwayaround.org.uk>
2014-03-11 10:58:31 +0000

    Fix issue in IE due to use of reserved word 'class'

--- external-links.js
+++ external-links.js
@@ -2,7 +2,7 @@
     $.fn.externalLinks = function(options) {
         options = options || {};
         var re = options.re || new RegExp(window.location.hostname.split('.').join('\\.'), 'i'),
-            cssClass = options.hasOwnProperty('class') ? options.class : 'external-link';
+            cssClass = options.hasOwnProperty('class') ? options['class'] : 'external-link';
         return this.find('a').filter(function () {
             var re = /^http(s)?/i
             return re.test(this.href);
